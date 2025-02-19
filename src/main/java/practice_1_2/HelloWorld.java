7af2d33f2e2bc6a114539d13b2e2e561a46a7691
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2025-02-19 22:05:59 +0200

    practice 1-1 & practice 1-2

---
+++ src/main/java/practice_1_2/HelloWorld.java
@@ -0,0 +1,7 @@
+package practice_1_2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
---
+++ src/main/java/sandbox/Sandbox.java
@@ -0,0 +1,7 @@
+package sandbox;
+
+public class Sandbox {
+    public static void main(String[] args) {
+
+    }
+}
